405aaefd32383739fe9c801907a8d1c5b7cdd055
Julian G <julian.gonzalez@myagi.com>
2021-11-15 12:53:26 -0300

    chore: testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-api_url="https://pokeapi.co/api/v2/pokemon/${INPUT_POKEMON_ID}"
-echo $api_url
-
-pokemon_name=$(curl "${api_url}" | jq ".name")
-echo $pokemon_name
-
-echo "::set-output name=pokemon_name::$pokemon_name"
+echo "🔎 The name of your branch is ${{ github.ref }} and your repository is ${{ github.repository }}."
+echo "ENV var:"
+env
