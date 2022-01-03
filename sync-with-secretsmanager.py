402d75e46a297ce32f9bc2f486f67cba33633abe
Julian G <79880978+juliandgon@users.noreply.github.com>
2022-01-03 16:25:12 -0300

    fix: corrected ascii content in properties (#5)
    * chore: added pattern removal
    
    * chore: refactored some code
    
    * chore: fixed code
    
    * chore: python script standardization
    
    * fix: corrected output generated by github toJson
    
    * fix: added exception handling for boto3
    
    * fix: corrected wrong quoting for secret file

--- sync-with-secretsmanager.py
+++ sync-with-secretsmanager.py
@@ -1,10 +1,11 @@
 
 import os
 import logging
-import json,yaml
+import json,yaml,re
 import argparse
 import traceback,sys
 from boto3 import Session
+from botocore.exceptions import ClientError
 
 def setLogger():
     # Setting logging level
@@ -53,26 +54,7 @@ def get_secret(secret_name):
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+        raise
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
@@ -84,75 +66,71 @@ def get_secret(secret_name):
     
     return json.loads(secret)
 
+def sync_secrets(secret_arn,secret_file,include_filter,exclude_filter,remove_pattern,dry_run=False):
+    # Get the Json content from the file with the secrets to be synced
+    with open(secret_file) as parsingFile:    
+        try: 
+            newValues = yaml.load(parsingFile, yaml.SafeLoader) # Load through YAML for safe loading
+        except yaml.parser.ParserError as error:
+            logging.info(f"failed to parse json, assuming an almost json... trying custom parsing")
+            with open(secret_file) as parsingFile:    
+                jsonfile = parsingFile.read()
+            jsonCorrected = re.sub(r"([\t\s]*)(.*):\s([^,\n]*)(,?)", r'\1"\2": "\3"\4', jsonfile)
+            newValues = json.loads(jsonCorrected) # Load through YAML for safe loading
+        
+        
+        # If a include filter parameter was used, only get the matching secrets for syncing
+        if include_filter:
+            check_value_in_list = lambda x: any([f in x[0] for f in include_filter])
+            newValues = dict(filter(check_value_in_list , newValues.items()))
+            
+        # If a exclude filter parameter was used, remove matching secrets for syncing
+        if exclude_filter:
+            check_value_not_in_list = lambda x: not any([f in x[0] for f in exclude_filter])
+            newValues = dict(filter(check_value_not_in_list , newValues.items()))
+            
+        if remove_pattern:
+            jsonDataFilter = {}
+            for patternToRemove in remove_pattern:
+                for key in newValues:
+                    keyModified = key.replace(patternToRemove, '')
+                    jsonDataFilter[keyModified] = newValues[key]
+                                
+            newValues = jsonDataFilter
 
-setLogger()
-
-parser = argparse.ArgumentParser("sync-with-secretsmanager")
-parser.add_argument('--secret_arn',required=True, nargs='?', help='AWS secret manager ARN  help')
-parser.add_argument('--secret_json_file',required=True, nargs='?', help='secret json file help')
-parser.add_argument('--include_filter_match', type=str, nargs='?', help='Include only filter for matching sequence')
-parser.add_argument('--exclude_filter_match', type=str, nargs='?', help='Exclude filter for matching sequence')
-parser.add_argument('--remove_pattern', type=str, nargs='?', help='Remove matching pattern in keys values')
-parser.add_argument('--dryrun', nargs='?', help='Dry run')
-args = parser.parse_args()
-secretARN=args.secret_arn
-secretsJsonFile=args.secret_json_file
-include_only_filter=args.include_filter_match
-include_only_filter = list(filter(None,[s.strip() for s in include_only_filter.split(",")]))
-exclude_filter=args.exclude_filter_match
-exclude_filter = list(filter(None, [s.strip() for s in exclude_filter.split(",")]))
-remove_pattern=args.remove_pattern
-remove_pattern = list(filter(None, [s.strip() for s in remove_pattern.split(",")]))
-_dryRun=args.dryrun
-
-
-# Get the Json content from the file
-with open(secretsJsonFile) as f: 
-    jsonData = yaml.load(f, yaml.SafeLoader) # Load thourhg YAML for safe loading
-    
-    # If a include filter parameter was used, then only add the matching secrets for syncing
-    if include_only_filter:
-        jsonDataFilter = {}
-        for key in jsonData:
-            matched = False
-            for filterMatch in include_only_filter:
-                if filterMatch in key:
-                    matched=True
-                    jsonDataFilter[key] = jsonData[key]
-                    break         
-        jsonData = jsonDataFilter
-
-    # If a prefix filter parameter was used, then only add the matching secrets for syncing
-    if exclude_filter:
-        jsonDataFilter = {}
-        for key in jsonData:
-            matched = False
-            for filterMatch in exclude_filter:
-                if filterMatch in key:
-                    matched=True
-                    break
-            if not matched:
-                jsonDataFilter[key] = jsonData[key]
-                        
-        jsonData = jsonDataFilter
-
-    if remove_pattern:
-        jsonDataFilter = {}
-        for patternToRemove in remove_pattern:
-            for key in jsonData:
-                keyModified = key.replace(patternToRemove, '')
-                jsonDataFilter[keyModified] = jsonData[key]
-                            
-        jsonData = jsonDataFilter
-
-    jsonString = json.dumps(jsonData)
+        oldValues = get_secret(secret_arn)
+        oldValues.update(newValues)
+        
+        jsonString = json.dumps(oldValues)
 
 
+    result = update_secret(secret_arn=secret_arn,jsonString=jsonString,DryRun=dry_run)
+    if result > 0:
+        logging.error(f"ERROR. Could not update secret manager entry")
+        exit(result)
 
-result = update_secret(secret_arn=secretARN,jsonString=jsonString,DryRun=_dryRun)
-if result > 0:
-    logging.error(f"ERROR. Could not update secret manager entry")
-    exit(result)
+    logging.info(f"AWS Secret Manager entry synced succesfully!")
+    return 0
 
-logging.info(f"AWS Secret Manager entry synced succesfully!")
-exit(0)
+if __name__=='__main__':
+    setLogger()
+    parser = argparse.ArgumentParser("sync-with-secretsmanager")
+    parser.add_argument('--secret_arn',required=True, nargs='?', help='AWS secret manager ARN  help')
+    parser.add_argument('--secret_json_file',required=True, nargs='?', help='secret json file help')
+    parser.add_argument('--include_filter_match', type=str, nargs='?', help='Include only filter for matching sequence')
+    parser.add_argument('--exclude_filter_match', type=str, nargs='?', help='Exclude filter for matching sequence')
+    parser.add_argument('--remove_pattern', type=str, nargs='?', help='Remove matching pattern in keys values')
+    parser.add_argument('--dryrun', nargs='?', help='Dry run')
+    args = parser.parse_args()
+    secretARN=args.secret_arn
+    secretsJsonFile=args.secret_json_file
+    include_filter=args.include_filter_match
+    include_filter = list(filter(None,[s.strip() for s in include_filter.split(",")]))
+    exclude_filter=args.exclude_filter_match
+    exclude_filter = list(filter(None, [s.strip() for s in exclude_filter.split(",")]))
+    remove_pattern=args.remove_pattern
+    remove_pattern = list(filter(None, [s.strip() for s in remove_pattern.split(",")]))
+    _dryRun=args.dryrun
+
+    rc = sync_secrets(secret_arn=secretARN,secret_file=secretsJsonFile,include_filter=include_filter,exclude_filter=exclude_filter,remove_pattern=remove_pattern,dry_run=_dryRun)
+    exit(rc)
